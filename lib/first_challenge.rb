d9e32e698d3f2a220c94614611afbd47aea13840
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-08-13 12:19:16 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,7 @@ def first_challenge
   }
 
   contacts.each do |person, data|
+    if data == "Freddy Mercury"
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
